c105f8f9e4ea337587d608b68ca50a9a62035092
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 09:02:20 +0800

    refactor: add the sum to the output

--- 001-nested-ternaries/index.js
+++ 001-nested-ternaries/index.js
@@ -9,11 +9,11 @@ function getSumv1(augend, addend){
   
   // Instead of this
   if(sum > 0){
-    console.log('V1: The sum is a positive number.')
+    console.log(`V1: The sum is ${sum} so it is a positive number.`)
   } else if(sum < 0){
-    console.log('V1: The sum is a negative number.')
+    console.log(`V1: The sum is ${sum} so it is a negative number.`)
   } else {
-    console.log('V1: The sum is zero.')
+    console.log(`V1: The sum is zero.`)
   }
 }
 
@@ -22,14 +22,16 @@ function getSumv2(augend, addend){
   
   // Do this
   (sum > 0) ?
-    console.log('V2: The sum is a positive number.')
+    console.log(`V2: The sum is ${sum} so it is a positive number.`)
 : (sum < 0) ?
-    console.log('V2: The sum is a negative number.') 
+    console.log(`V2: The sum is ${sum} so it is a negative number.`) 
 :  
-    console.log('V2: The sum is zero.')
+    console.log(`V2: The sum is zero.`)
 }
 
 getSumv1(20, 4);
 getSumv2(20, 4);
-getSumv1(3, 14);
-getSumv2(3, 14);
+getSumv1(-3, 3);
+getSumv2(-3, 3);
+getSumv1(43, -44);
+getSumv2(43, -44);
