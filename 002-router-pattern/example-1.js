d5ccb7990582aa107c53aa2913e048d8b1724e7c
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 09:51:24 +0800

    chore: add calls for output

---
+++ 002-router-pattern/example-1.js
@@ -0,0 +1,4 @@
+console.log( responder('list hikes') );
+console.log( responder('recommend hike') );
+console.log( responder('add hike Mirror Lake') );
+console.log( responder('where is Mirror Lake') );
