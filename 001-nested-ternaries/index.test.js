b42292799e65d5d50627e7970d7430b50555f46f
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 09:22:26 +0800

    chore: add more test to the sum function

--- 001-nested-ternaries/index.test.js
+++ 001-nested-ternaries/index.test.js
@@ -15,4 +15,30 @@ describe('the sum function that has a nested ternaries', () => {
 
     consoleSpy.mockRestore();
   });
+
+  // Should correctly add two negative numbers and print the sum as a negative number
+  it('should correctly add two negative numbers and print the sum as a negative number', () => {
+    const augend = -5;
+    const addend = -10;
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    getSumv2(augend, addend);
+
+    expect(consoleSpy).toHaveBeenCalledWith(`V2: The sum is -15 so it is a negative number.`);
+
+    consoleSpy.mockRestore();
+  });
+
+  // Should correctly add a positive and a negative number and print the sum
+  it('should correctly add a positive and a negative number and print the sum', () => {
+    const augend = 5;
+    const addend = -10;
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    getSumv2(augend, addend);
+
+    expect(consoleSpy).toHaveBeenCalledWith(`V2: The sum is -5.`);
+
+    consoleSpy.mockRestore();
+  });
 })
