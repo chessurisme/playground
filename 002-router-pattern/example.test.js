88bc7561e449697f399c87d2620f808f29741ed9
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 10:46:50 +0800

    refactor: change test & remove the commented line

--- 002-router-pattern/example.test.js
+++ 002-router-pattern/example.test.js
@@ -7,40 +7,55 @@ let hikes = [
   'Canyon Creek Meadows',
 ];
 
-let randomHike = () =>
-  hikes[Math.floor(Math.random() * hikes.length)];
+let randomHike = () => hikes[Math.floor(Math.random() * hikes.length)];
 
-describe('router pattern for example 1', () => {
-  // Returns the expected response for the 'list hikes' command.
-  it('should return the expected response for the "list hikes" command', () => {
+describe('responder', () => {
+  // Returns the correct response for a valid 'list hikes' command.
+  it('should return the correct response for a valid \'list hikes\' command', () => {
     const message = 'list hikes';
     const expectedResponse = hikes.join('\n');
     const actualResponse = responder(message);
     expect(actualResponse).toEqual(expectedResponse);
   });
 
-  // Returns the expected response for the 'recommend hike' command.
-  it('should return the expected response for the "recommend hike" command', () => {
+  // Returns the correct response for a valid 'recommend hike' command.
+  it('should return the correct response for a valid \'recommend hike\' command', () => {
     const message = 'recommend hike';
     const expectedResponse = `I recommend ${randomHike()}`;
     const actualResponse = responder(message);
     expect(actualResponse).toEqual(expectedResponse);
   });
 
-  // Returns the expected response for the 'add hike' command when given a valid message.
-  it('should return the expected response for the "add hike" command when given a valid message', () => {
+  // Returns the correct response for a valid 'add hike' command.
+  it('should return the correct response for a valid \'add hike\' command', () => {
     const message = 'add hike Mount Everest';
     const hike = 'Mount Everest';
+    hikes.push(hike);
     const expectedResponse = `Added ${hike}!`;
     const actualResponse = responder(message);
     expect(actualResponse).toEqual(expectedResponse);
-    expect(hikes).toContain(hike);
   });
 
-  // Returns the default response for a message that does not match any command.
-  it('should return the default response for a message that does not match any command', () => {
-    const message = 'invalid command';
+  // Returns the default response for a command that does not match any known commands.
+  it('should return the default response for a command that does not match any known commands', () => {
+    const message = 'unknown command';
     const expectedResponse = "Sorry, I don't understand that.";
+    const actualResponse = responder(message);
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  // Returns an error if the 'add hike' command is missing the hike name.
+  it('should return an error if the \'add hike\' command is missing the hike name', () => {
+    const message = 'add hike';
+    const expectedResponse = 'Error: Missing hike name.';
+    const actualResponse = responder(message);
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  // Returns an error if the 'add hike' command has an empty hike name.
+  it('should return an error if the \'add hike\' command has an empty hike name', () => {
+    const message = 'add hike ';
+    const expectedResponse = 'Error: Empty hike name.';
     const actualResponse = responder(message);
     expect(actualResponse).toEqual(expectedResponse);
   });
