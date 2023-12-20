42ec4bf53f8e1836f862d95b3d880827e6d5f287
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 09:57:06 +0800

    chore: add 3 if-else cases and one fallback
    Add an array of places to hike, and add 4 behaviors like listing the hikes, recommend hikes, adding hikes, and a fallback.

--- 002-router-pattern/example-1.js
+++ 002-router-pattern/example-1.js
@@ -1,3 +1,25 @@
+let hikes = [
+  'Lost Lake',
+  'Canyon Creek Meadows',
+];
+
+let randomHike = () =>
+  hikes[Math.floor(Math.random() * hikes.length)];
+
+let responder = (message) => {
+  if (message === 'list hikes') {
+    return hikes.join('\n');
+  } else if (message === 'recommend hike') {
+    return `I recommend ${randomHike()}`;
+  } else if (message.startsWith('add hike')) {
+    let hike = message.slice(9);
+    hikes.push(hike);
+    return `Added ${hike}!`;
+  }
+
+  return "Sorry, I don't understand that.";
+};
+
 console.log( responder('list hikes') );
 console.log( responder('recommend hike') );
 console.log( responder('add hike Mirror Lake') );
