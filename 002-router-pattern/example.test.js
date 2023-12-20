eee04a9d038bdf4b9e01005a9e68e8bb32219056
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-20 10:26:58 +0800

    chore: add suite for testing the router pattern

---
+++ 002-router-pattern/example.test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+import { responderv3 } from "./example-1.js";
+
+describe('router pattern for example 1', () => {
+
+  // Returns the expected response for the 'list hikes' command.
+  it('should return the expected response for the "list hikes" command', () => {
+    const message = 'list hikes';
+    const expectedResponse = hikes.join('\n');
+    const actualResponse = responderv3(message);
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  // Returns the expected response for the 'recommend hike' command.
+  it('should return the expected response for the "recommend hike" command', () => {
+    const message = 'recommend hike';
+    const expectedResponse = `I recommend ${randomHike()}`;
+    const actualResponse = responderv3(message);
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  // Returns the expected response for the 'add hike' command when given a valid message.
+  it('should return the expected response for the "add hike" command when given a valid message', () => {
+    const message = 'add hike Mount Everest';
+    const hike = 'Mount Everest';
+    const hikes = [];
+    const expectedResponse = `Added ${hike}!`;
+    const actualResponse = responderv3(message);
+    expect(actualResponse).toEqual(expectedResponse);
+    expect(hikes).toContain(hike);
+  });
+
+  // Returns the default response for a message that does not match any command.
+  it('should return the default response for a message that does not match any command', () => {
+    const message = 'invalid command';
+    const expectedResponse = "Sorry, I don't understand that.";
+    const actualResponse = responderv3(message);
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+});
