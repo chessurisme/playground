4f6983c52e3e91cc4a3413779ab7e15f24ff6f0f
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-23 08:05:22 +0800

    chore: add test suites

---
+++ vanilla-javascript/design-patterns/002-router-patterns/example-1.test.js
@@ -0,0 +1,35 @@
+import { responder, hikes } from './example-1.js';
+
+describe('Responder Function', () => {
+  let originalHikes;
+
+  beforeAll(() => {
+    originalHikes = [...hikes];
+  });
+
+  afterAll(() => {
+    hikes = [...originalHikes];
+  });
+
+  test('List Hikes Command', () => {
+    const result = responder('list hikes');
+    expect(result).toEqual(originalHikes.join('\n'));
+  });
+
+  test('Recommend Hike Command', () => {
+    const result = responder('recommend hike');
+    expect(result).toMatch(/^I recommend (Lost Lake|Canyon Creek Meadows)$/);
+  });
+
+  test('Add Hike Command', () => {
+    const newHike = 'Mirror Lake';
+    const result = responder(`add hike ${newHike}`);
+    expect(result).toBe(`Added ${newHike}!`);
+    expect(hikes).toContain(newHike);
+  });
+
+  test('Unknown Command', () => {
+    const result = responder('where is Mirror Lake');
+    expect(result).toBe(`Sorry, I don't understand "where is Mirror Lake".`);
+  });
+});
