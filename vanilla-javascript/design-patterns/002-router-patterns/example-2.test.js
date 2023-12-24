963cdefcfe21affe6826ecad9785ab95e12fe4e7
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2023-12-25 07:31:06 +0800

    chore: add test for example-2

--- vanilla-javascript/design-patterns/002-router-patterns/example-2.test.js
+++ vanilla-javascript/design-patterns/002-router-patterns/example-2.test.js
@@ -1,3 +1,30 @@
 "use strict";
 
 import { responder, fruits } from './example-2.js';
+
+describe('Responder Function', () => {
+  let originalFruits;
+
+  beforeAll(() => {
+    originalFruits = [...fruits];
+  });
+
+  afterAll(() => {
+    fruits = [...originalFruits];
+  });
+
+  test('List Fruits Command', () => {
+    const result = responder('list fruits');
+    expect(result).toEqual(originalFruits.join('\n'));
+  });
+
+  test('Random Fruit Command', () => {
+    const result = responder('random fruit');
+    expect(fruits).toContain(result);
+  });
+
+  test('Unknown Command', () => {
+    const result = responder('add banana');
+    expect(result).toBe('Sorry, I don\'t understand "add banana".');
+  });
+});
