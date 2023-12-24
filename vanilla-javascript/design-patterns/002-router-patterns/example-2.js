69da80564751f6a2a2a8193ac3839b753866f408
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-25 06:46:01 +0800

    chore: add fruits variable

---
+++ vanilla-javascript/design-patterns/002-router-patterns/example-2.js
@@ -0,0 +1,16 @@
+let fruits = [
+  'apple', 
+  'banana', 
+  'orange',
+  'grape',
+  'strawberry',
+  'pineapple',
+  'mango',
+  'watermelon',
+  'cherry',
+  'blueberry',
+  'kiwi',
+  'coconut',
+  'avocado'
+];
+
