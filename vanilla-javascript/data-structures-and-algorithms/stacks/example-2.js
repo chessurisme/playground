ff7d73485e69b2fcf1f16053bd134a094800aa9b
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-01-01 16:18:28 +0800

    feat: add class of Stack

---
+++ vanilla-javascript/data-structures-and-algorithms/stacks/example-2.js
@@ -0,0 +1,37 @@
+"use strict";
+
+class Stack {
+  constructor(){
+    this.count = 0;
+    this.storage = {};
+    this.push = push();
+    this.pop = pop();
+    this.size = size();
+    this.peek = peek();
+  }
+
+  // Adds a value onto the end  of the stack
+  push(value){
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // Removes and returns the value at the end of the stack
+  pop(){
+    this.count == 0 ? undefined : null;
+
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  } 
+
+  size(){
+    return this.count;
+  }
+
+  // Returns the value at the end of the stack
+  peek(){
+    return this.storage[this.count - 1];
+  }
+}
